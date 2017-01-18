1b8348fca17fbf1d8a766c1a6bce2484d575d9f8
thomaslawton91 <thomaslawton91@gmail.com>
2017-01-18 15:26:15 -0500

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+# Create posts
+50.times do
+
+  Post.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+# Create comments
+100.times do
+  Comment.create!(
+  post: posts.sample,
+  body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"

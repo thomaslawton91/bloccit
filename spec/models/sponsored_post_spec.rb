882b2cd029378ac40ff174640c42c98843ca3d9c
thomaslawton91 <thomaslawton91@gmail.com>
2017-01-30 20:53:40 -0500

    Save work

---
+++ spec/models/sponsored_post_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe SponsoredPost, type: :model do
+  let(:name) { RandomData.random_sentence }
+  let(:description) { RandomData.random_paragraph }
+
+let(:topic) { Topic.create!(name: name, description: description) }
+let(:sponsored_post) { topic.sponsored_posts.create!(title: "Title", body: "Body", price: 0) }
+
+# #1
+  describe "attributes" do
+    it "has expected attributes" do
+      expect(sponsored_post).to have_attributes(title: "Title", body: "Body", price: 0)
+    end
+  end
+end

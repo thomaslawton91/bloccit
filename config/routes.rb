a549753601f21c2333cf450052aa831d797440f3
thomaslawton91 <thomaslawton91@gmail.com>
2017-01-31 15:08:48 -0500

    Create user model via TDD. Update to reflect changes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  
+
   resources :topics do
     resources :posts, except: [:index]
   end
+
+  resources :users, only: [:new, :create]
 
   get 'about' => 'welcome#about'
 

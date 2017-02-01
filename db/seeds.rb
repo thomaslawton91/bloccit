e2798ee493e8b022ae59c8b4ebe89e6ba2389423
thomaslawton91 <thomaslawton91@gmail.com>
2017-01-31 20:25:58 -0500

    Associate posts with users. Update post and topic controllers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
 
+#Create Users
+5.times do
+  User.create!(
+  name: RandomData.random_name,
+  email: RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -20,6 +30,7 @@ topics = Topic.all
 50.times do
 
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -35,7 +46,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'thomaslawton91@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
